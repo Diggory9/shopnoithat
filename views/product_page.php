1db80020090f37852623de8a38133dc80f3459ca
viet22nph <viet22nph@gmail.com>
2023-12-19 23:47:54 +0700

    giỏ hàng

--- views/product_page.php
+++ views/product_page.php
@@ -1,6 +1,6 @@
-<div class="container-fluid mt-5 mb-5">
+<div class="container mt-5 mb-5">
     <div class="row g-2">
-        <div class="col-md-3 bg-light">
+        <div class="col-md-2 bg-light">
             <div class="p-2">
                 <h6 class="text-uppercase text-center">Danh mục sản phẩm</h6>
                 <div class="p-lists">
@@ -15,7 +15,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-10">
             <div class="row g-2">
 
             <!-- show product -->
@@ -26,10 +26,11 @@
                     ?>
                     <div class="col-lg-4">
                         <div class="product py-4">
-                            <div class="text-center"> <img src="<?php echo BASE__URL.'images/uploads/'. $value->images[0]->image_path;?>" width="200" height="200"> </div>
+                            <div class="text-center"> <img src="<?php echo BASE__URL.'images/uploads/'. $value->images[0]->image_path;?>" width="250" height="200"> </div>
                             <div class="about text-center">
                             <!-- show name product     -->
                             <h5 height="70"></h5><?php echo $value->product_name;?></h5>
+                            <br/>
                             <!-- show price product  -->
                             <span><?php 
                                  $priceFormat = number_format($value->product_price, 2, ',', '.');
@@ -38,8 +39,8 @@
                             </div>
                             <div class="cart-button mt-3 px-2 d-flex justify-content-between align-items-center">
                                 <button  class="btn btn-primary text-uppercase"><a href="/detail-product?id=<?php echo $value->product_id?>" class="text-decoration-none text-white">CHI TIẾT SẢN PHẨM</a></button>
-                                <button class=" btn btn-primary"><a class="product_fav"><i
-                                            class="bi bi-cart3"></i></a></button>
+                                <a href="add-cart?id=<?php echo $value->product_id?>">  <button class=" btn btn-primary"><i class="fa-brands fa-opencart"></i></button></a>
+                               
                             </div>
                         </div>
                     </div>
