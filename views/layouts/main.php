a7393eec451a27b770f86387e1ae5deee4aef293
viet22nph <viet22nph@gmail.com>
2023-12-19 23:54:12 +0700

    fixx

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,7 +30,9 @@ use app\core\Application;
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 		<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
 		<link href="css/tiny-slider.css" rel="stylesheet">	
-		<link href="css/style.css" rel="stylesheet">
+		<link href="<?php echo BASE__URL?>css/admin.css" rel="stylesheet" />
+    <link href="<?php echo BASE__URL?>css/style.css" rel="stylesheet"/>
+    <link href="<?php echo BASE__URL?>css/detailproduct.css" rel="stylesheet">
 		<script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
 	</head>
@@ -92,6 +94,8 @@ use app\core\Application;
 		<script src="js/bootstrap.bundle.min.js"></script>
 		<script src="js/tiny-slider.js"></script>
 		<script src="js/custom.js"></script>
+		<script src="<?php echo BASE__URL?>js/datatables-simple-demo.js"></script>
+    <script src="<?php echo BASE__URL?>js/detailproduct.js"></script>
 		<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
 	</body>
 
